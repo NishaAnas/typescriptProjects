7adaa121fd7319f9c1e49da9a3d6e55f32b827b1
nishamashhood <mashhoodnisha@gmail.com>
2024-09-08 21:38:07 +0530

    final Changes in StudManagement

--- type script/studentManagement/dist/models/student.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const StudentSchema = new mongoose_1.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    class: { type: String, required: true }
-});
-exports.default = mongoose_1.default.model('Student', StudentSchema);
